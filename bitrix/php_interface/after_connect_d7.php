2966de4cfa330971b2ebaccd5f5b24b563927f2b
Lucian <applancer312@gmail.com>
2018-03-02 23:35:34 -0300

    usetting up sql_mode

--- bitrix/php_interface/after_connect_d7.php
+++ bitrix/php_interface/after_connect_d7.php
@@ -1,5 +1,5 @@
 <?
 $connection = \Bitrix\Main\Application::getConnection();
-$connection->queryExecute("SET sql_mode='ALLOW_INVALID_DATES'");
+$connection->queryExecute("SET sql_mode=''");
 $connection->queryExecute("SET LOCAL time_zone='".date('P')."'");
 ?>
